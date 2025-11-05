67765d6f5951e3748ada5ed1ba08d2bb8026cd9d
Murilo Puhl Fronza <murilo.fronza@compasso.com.br>
2025-11-05 16:59:27 -0300

    Adiciona teste de soma

--- calculator.spec.js
+++ calculator.spec.js
@@ -5,6 +5,10 @@ test("Deve retornar 4 quando somar 2 + 2", () => {
 expect(calculator.add(2, 2)).toBe(4);
 })
 
+test("Deve retornar 10 quando somar 5 + 5", () => {
+  expect(calculator.add(5, 5)).toBe(10);
+});
+
 
 test("Deve retornar 2 quando subtrair 4 - 2", () => {
   expect(calculator.sub(4,2)).toBe(2);
